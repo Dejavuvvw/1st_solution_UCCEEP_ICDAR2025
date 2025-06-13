922fe977ffc34fe2817ea7b44dc36a753bcfc3f4
dejavuvvw <dejavuvvw@gmail.com>
2025-06-13 10:42:25 +0800

    update train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,37 +1,39 @@
-nnodes=8
-nproc_per_node=8
+# 80GB
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-MAX_PIXELS=1003520 \
-CUDA_VISIBLE_DEVICES=0,1,2,3,5,6,7 \
+NNODES=8
+NODE_RANK=0
+MASTER_ADDR="0.0.0.0"
+MASTER_PORT=8999
+
+which torchrun
 torchrun \
-    --master_port 29500 \
-    --nproc_per_node=$nproc_per_node \
-    --nnodes=$nnodes \
-    --node_rank=1 \
-    --master_addr=xxx.xxx.xxx.xxx \
-    swift/cli/sft.py \
-    --model Qwen/Qwen2.5-VL-3B-Instruct \
-    --train_type full \
-    --dataset 'xxxxxx' \
-    --torch_dtype bfloat16 \
-    --num_train_epochs 10 \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 32 \
+    --nproc_per_node 8 \
+    --nnodes="${NNODES}" \
+    --node_rank="${NODE_RANK}" \
+    --master_addr="${MASTER_ADDR}" \
+    --master_port="${MASTER_PORT}" \
+    xxx/lib/python3.10/site-packages/swift/cli/sft.py \
+    --model Qwen/Qwen2.5-VL-7B-Instruct \
+    --dataset ./questions_ocr_promptnew_add_prompt_250408.jsonl \
     --learning_rate 6e-6 \
-    --gradient_accumulation_steps $(expr 32 / $nproc_per_node / $nnodes) \
-    --eval_steps 100 \
-    --save_steps 100 \
-    --save_total_limit 2 \
-    --logging_steps 5 \
-    --output_dir output \
-    --warmup_ratio 0.05 \
-    --dataloader_num_workers 16 \
-    --deepspeed zero3-offload \
-    --max_new_token 3000 \
+    --train_type full \
+    --deepspeed zero3_offload \
     --attn_impl flash_attn \
-    --custom_data_aug \
-    --dynamic_ocr \
+    --freeze_vit \
+    --output_dir ./output/ \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 1 \
+    --save_steps 100 \
+    --logging_steps 1 \
+    --max_new_token 5000 \
+    --dataloader_num_workers 4 \
+    --gradient_accumulation_steps 8 \
+    --max_pixels 1003520 \
+    --custom_data_aug true \
+    --dynamic_ocr true \
     --mode0_prob 0.9 \
     --num_img_to_ocr 1 \
-    --remove_unused_columns false \
-    --seed 42
+    --remove_unused_columns False \
+    --split_dataset_ratio 0.0 \
+    
